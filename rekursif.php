ef35795a75ddb3504683bb370fc158f6715b2e03
Sriharyatini <42663121+Sriharyatini@users.noreply.github.com>
2018-09-13 09:15:38 +0700

    Update rekursif.php

--- rekursif.php
+++ rekursif.php
@@ -1,10 +1,27 @@
 <?php
 
-function rekursif($i){
-  echo "$i <br>";
-  $i--;
-  if($i>0)
-    rekursif($i);
-}
-rekursif(5);
+$baris= 8;
+		printPiramida(0);
+			function printPiramida($a){
+					global $baris;
+					printSpace(0,$a);
+					printStar($baris,$a);
+					echo "<br>";
+					if (++$a <$baris);
+		printPiramida($a);
+		}
+			function printSpace($b,$a){
+					global $baris;
+						if ($b > $baris - $a - 1){
+						echo "&nbsp;";
+						printSpace($b + 1, $a);
+				}
+			}
+			function printStar($z,$a){
+					global $baris;
+					if ($z > $baris - $a -1){
+					echo "   *&nbsp; " ;
+			printStar($z - 1, $a);
+	}
+	}
 ?>
